48e67e4d471597e516acc4da1f3b791148969490
Danni <89629702+TwelfthDannis@users.noreply.github.com>
2024-02-19 15:26:05 +0700

    test

--- src/app/(site)/menu/page.js
+++ src/app/(site)/menu/page.js
@@ -4,7 +4,7 @@ import Logout from "@/app/(site)/menu/components/logout";
 import "./style/menu.css"
 
 
-export const Page=()=>{
+const Page=()=>{
     return(
         <aside className={`-translate-x-full" absolute flex top-0 left-0 z-40 w-screen h-dvh transition-transform lg:translate-x-0 lg:relative lg:h-full lg:w-auto`}>
             <div className="h-full mr-2 px-3 py-4 bg-zinc-950 w-48 border-r border-zinc-800 flex flex-col justify-between lg:w-auto">
@@ -16,4 +16,4 @@ export const Page=()=>{
         </aside>
     )
 }
-export default Page
+export default Page;
